3e4e7997b27ac4e59fbfe4d63b93856c0a2c1c0c
Andrei Dvoynos <ajdvoynos@gmail.com>
2021-07-12 21:52:54 +0200

    Clone from github template

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,61 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app" class="w-full h-full flex flex-col justify-center bg-gray-300">
+    <div
+      v-if="showPasswordReset"
+      class="w-full h-full flex flex-col justify-center items-center p-4"
+    >
+      <PasswordReset />
+    </div>
+    <div
+      v-else-if="userSession === null"
+      class="w-full h-full flex flex-col justify-center items-center p-4"
+    >
+      <Auth />
+    </div>
+    <div v-else class="w-full h-full flex flex-col justify-center items-center p-4 max-w-sm m-auto">
+      <Suspense>
+        <template #default>
+          <div>
+            <TodoList />
+            <button class="btn-black w-full mt-12" @click="handleLogout">
+              Logout
+            </button>
+          </div>
+        </template>
+        <template #fallback>
+          <Loading />
+        </template>
+      </Suspense>
+    </div>
+    <Footer />
   </div>
-  <router-view/>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Auth from '@/components/Auth.vue'
+import PasswordReset from '@/components/PasswordReset.vue'
+import TodoList from '@/components/TodoList.vue'
+import Loading from '@/components/Loading.vue'
+import Footer from '@/components/Footer.vue'
+import { userSession, handleLogout } from '@/vuetils/useAuth'
+import { getParameterByName } from '@/lib/helpers'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default {
+  components: {
+    Auth,
+    PasswordReset,
+    TodoList,
+    Loading,
+    Footer,
+  },
+  computed: {
+    showPasswordReset: function() {
+      const requestType = getParameterByName('type', location.href)
+      return requestType === 'recovery'
     }
-  }
+  },
+  setup() {
+    return { userSession, handleLogout }
+  },
 }
-</style>
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import { supabase } from './lib/supabase'
+import { userSession } from '@/vuetils/useAuth'
+import './assets/tailwind.css'
 
-createApp(App).use(router).mount('#app')
+createApp(App).mount('#app')
+
+/**
+ * Keeps track of if the user is logged in or out and will update userSession state accordingly.
+ */
+supabase.auth.onAuthStateChange((event, session) => {
+  userSession.value = session
+})
